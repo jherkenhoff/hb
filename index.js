07cb38b43c0a33a45f7fa493724a7aa7cc5b266e
mr-kenhoff <mrkenhoff+github@mailbox.org>
2020-04-10 15:33:31 +0200

    Test geolocation

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("It works")
+
+
+if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(locationCallback);
+} else {
+    var x = document.getElementById("geolocation-error");
+    x.innerHTML = "Oh, mist.. Da funktioniert was nicht. Probiers mal mit einem anderen Gerät..";
+}
+
+
+
+
+function locationCallback(position) {
+    console.log(position);
+}
