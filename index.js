55c6ee7e97d682533763ad9f881902c45078e529
mr-kenhoff <mrkenhoff+github@mailbox.org>
2020-04-10 15:51:00 +0200

    Test geolocation

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 
-
 if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(locationCallback);
+    navigator.geolocation.watchPosition(locationCallback, locationErrorCallback, {maximumAge: 0, enableHighAccuracy: true, timeout: 2000});
 } else {
     var x = document.getElementById("geolocation-error");
     x.innerHTML = "Oh, mist.. Da funktioniert was nicht. Probiers mal mit einem anderen Gerät..";
 }
 
-
-
+function locationErrorCallback(error) {
+    console.log("ERROR :(");
+    console.log(error);
+    var x = document.getElementById("geolocation-error");
+    x.innerHTML = "Oh, mist.. Da funktioniert was nicht. Probiers mal mit einem anderen Gerät..";
+}
 
 function locationCallback(position) {
     console.log(position);
+    var x = document.getElementById("geolocation-error");
+    x.innerHTML = position.coords.latitude;
 }
